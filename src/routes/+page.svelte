71c00f1ccde0461b5d3705fba6bdaefd75be94fa
chinasorrows <charizard.au@gmail.com>
2023-03-19 00:25:56 +0300

    routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {goto, beforeNavigate, afterNavigate} from "$app/navigation";
+
+    const handleClick = () => {
+        console.log("Placing Your Order!")
+        goto("/products")
+    }
+
+    beforeNavigate((navigation) =>{
+        console.log({before:navigation})
+    })
+    afterNavigate((navigation) =>{
+        console.log({after:navigation})
+    })
+</script>
+
+
+
+<h1>Welcome to Homepage!</h1>
+
+<a href="/blog">Blog!</a><br>
+<a href="/products">Products!</a><br><br>
+<button on:click={handleClick}> Place Order! </button>
